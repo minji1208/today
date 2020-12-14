aff035caf11be9ed548322e1addba4a3f19626e6
minji1208 <73093276+minji1208@users.noreply.github.com>
2020-12-15 07:10:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,23 @@
 'use strict';
-var id_week_number = document.getElementById('week-number');
-var id_date = document.getElementById('date');
+var ddaytimer = setInterval (dayGap, 1000)
+function dayGap () {
+    var dday = new Date("November 24, 2017 00:00:00").getTime();//디데이
+    var nowday = new Date();//현재
+    nowday = nowday.getTime();//밀리세컨드 단위변환
+    var distance = dday - nowday;//디데이에서 현재까지 뺀다.
 
-function isLeap(year) {
-    if (year % 400 == 0) return true;
-    if (year % 100 == 0) return false;
-    if (year % 4 == 0) return true;
-    return false;
+    var d = Math.floor(distance / (1000 * 60 * 60 * 24));//일
+
+    var h = Math.floor((distance / (1000*60*60)) % 24);//시간
+    var m = Math.floor((distance / (1000*60)) % 60);//분
+    var s = Math.floor((distance / 1000) % 60);//초
+
+    if (distance <= 0) {//당일넘어섰을때, dday로 변경
+        document.getElementById("dday-timer").innerHTML = "D-day";
+    } else {
+        document.getElementById("time-day").innerHTML = d;
+        document.getElementById("time-hour").innerHTML = h;
+        document.getElementById("time-minute").innerHTML = m;
+        document.getElementById("time-second").innerHTML = s;
+    }
 }
-
-function pYear(year) {
-    return (year + Math.floor(year / 4)
-                 - Math.floor(year / 100)
-                 + Math.floor(year / 400)) % 7;
-}
-
-function lastWeek(year) {
-    if (pYear(year) == 4 || pYear(year - 1) == 3)
-        return 53;
-    return 52;
-}
-
-function ordinalDay(date) {
-    var ordinal_table = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-
-    if (isLeap(date.getFullYear()))
-        for (var i = 2; i < ordinal_table.length; i++)
-            ordinal_table[i] += 1;
-
-    return ordinal_table[date.getMonth()] + date.getDate();
-}
-
-function weekNumber(date) {
-    var ordinal_day = ordinalDay(date);
-    var current_year = date.getFullYear();
-    var weekday = date.getDay();
-    var week = Math.floor((ordinal_day - weekday + 10) / 7);
-
-    if (week < 1) return lastWeek(current_year- 1);
-    if (week > lastWeek(current_year)) return 1;
-    return week;
-}
-
-function renderPage(date) {
-    var week_number = weekNumber(date);
-
-    id_week_number.innerText = week_number;
-    id_date.innerText = date.toLocaleString();
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    var date = new Date();
-    renderPage(date);
-    setInterval(function() {
-        var now = new Date();
-        id_date.innerText = now.toLocaleString();
-
-        if (now.getDate != date.getDate) {
-            date = now;
-            renderPage(date);
-        }
-    }, 1000);
-});
